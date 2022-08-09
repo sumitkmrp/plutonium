f2a47a5af6c65c55a72eedfa476a3e3cd5ab32d1
sumit <sumitkumar70806@gmail.com>
2022-08-09 20:00:14 +0530

    update my files

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,96 @@
 const express = require('express');
+const _ = require('underscore')
+
 const abc = require('../introduction/intro')
+const loggerModule = require('../logger/logger.js')
+const formatterModule = require('../validator/formatter')
+const helperModule = require('../util/helper')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
-    logger.welcome()
-
+    loggerModule.printInfo()
+    formatterModule.trimMyString()
+    formatterModule.getUpperCaseString()
+    formatterModule.changetoLowerCase()
+    helperModule.getTodaysDate()
+    helperModule.getCurrentMonth()
+    helperModule.printBatchDetails()
+    let weekdend = ['Saturday', 'Sunday', 'Monday']
+    let result = _.first(weekdend, 2)
+    console.log('Unserscore example resultr is ', result)
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+
+router.get('/get/moovies/:indexNumber', function (req, res) {
+    const movies = ['iron man', 'thor', 'captain america', 'the hulk', 'the avengers', 'black panther', 'end game', 'spiderman']
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+    //check index
+    if (movieIndex < 0 || movieIndex > movieIndex.length) {
+        return res.send('the index value is not correct, plz check it')
+    }
+
+    let requireMovie = movies[movieIndex]
+    res.send(requireMovie)
+
+
 })
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
+router.get("/films", function (req, res) {
+    const films = [
+        {
+            "id": 1,
+            "name": "the hulk"
+        },
+        {
+            "id": 2,
+            "name": "iron man"
+        },
+        {
+            "id": 3,
+            "name": "thor"
+        }
+    ]
+    res.send(films)
 })
 
+router.get("/films/:filmId", function (req, res) {
+    const films = [
+        {
+            "id": 1,
+            "name": "the hulk"
+        },
+        {
+            "id": 2,
+            "name": "iron man"
+        },
+        {
+            "id": 3,
+            "name": "thor"
+        }
+    ]
+    let filmId = req.params.filmId
+    for (let i = 0; i < films.length; i++) {
+        let movie = film[i]
+        if (film.id == filmId)
+            return res.send(film)
+    }
+    res.send("film id does not exist")
+    // console.log('filmId recieve is',filmId)
+})
+
+
+router.get('/test-you', function (req, res) {
+    res.send('This is the second routes implementation')
+})
+
+router.get('/give-me-students-data', function (req, res) {
+
+})
 module.exports = router;
+// adding this comment for no reason
+
+// hello world!
