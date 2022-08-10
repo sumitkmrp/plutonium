f8d52d4ff72af19514a25db93c18d8a17e79ac18
sumit <sumitkumar70806@gmail.com>
2022-08-10 15:01:20 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch session/get-api
    Your branch is up to date with 'origin/session/get-api'.
    
    Changes to be committed:
    	modified:   src/routes/route.js
    
    Changes not staged for commit:
    	modified:   node_modules/.package-lock.json
    	modified:   package-lock.json
    	modified:   package.json
    
    Untracked files:
    	node_modules/.bin/nodemon
    	node_modules/.bin/nodemon.cmd
    	node_modules/.bin/nodemon.ps1
    	node_modules/.bin/nodetouch
    	node_modules/.bin/nodetouch.cmd
    	node_modules/.bin/nodetouch.ps1
    	node_modules/.bin/nopt
    	node_modules/.bin/nopt.cmd
    	node_modules/.bin/nopt.ps1
    	node_modules/.bin/semver
    	node_modules/.bin/semver.cmd
    	node_modules/.bin/semver.ps1
    	node_modules/abbrev/
    	node_modules/anymatch/
    	node_modules/balanced-match/
    	node_modules/binary-extensions/
    	node_modules/brace-expansion/
    	node_modules/braces/
    	node_modules/chokidar/
    	node_modules/concat-map/
    	node_modules/fill-range/
    	node_modules/glob-parent/
    	node_modules/has-flag/
    	node_modules/ignore-by-default/
    	node_modules/is-binary-path/
    	node_modules/is-extglob/
    	node_modules/is-glob/
    	node_modules/is-number/
    	node_modules/minimatch/
    	node_modules/nodemon/
    	node_modules/nopt/
    	node_modules/normalize-path/
    	node_modules/picomatch/
    	node_modules/pstree.remy/
    	node_modules/readdirp/
    	node_modules/semver/
    	node_modules/simple-update-notifier/
    	node_modules/supports-color/
    	node_modules/to-regex-range/
    	node_modules/touch/
    	node_modules/undefsafe/

--- src/routes/route.js
+++ src/routes/route.js
@@ -24,72 +24,72 @@ router.get('/test-me', function (req, res) {
 });
 
 
-router.get('/get/moovies/:indexNumber', function (req, res) {
-    const movies = ['iron man', 'thor', 'captain america', 'the hulk', 'the avengers', 'black panther', 'end game', 'spiderman']
-    console.log(req.params.indexNumber)
-    let movieIndex = req.params.indexNumber
-    //check index
-    if (movieIndex < 0 || movieIndex > movieIndex.length) {
-        return res.send('the index value is not correct, plz check it')
-    }
+// router.get('/get/moovies/:indexNumber', function (req, res) {
+//     const movies = ['iron man', 'thor', 'captain america', 'the hulk', 'the avengers', 'black panther', 'end game', 'spiderman']
+//     console.log(req.params.indexNumber)
+//     let movieIndex = req.params.indexNumber
+//     //check index
+//     if (movieIndex < 0 || movieIndex > movieIndex.length) {
+//         return res.send('the index value is not correct, plz check it')
+//     }
 
-    let requireMovie = movies[movieIndex]
-    res.send(requireMovie)
-
-
-})
-
-router.get("/films", function (req, res) {
-    const films = [
-        {
-            "id": 1,
-            "name": "the hulk"
-        },
-        {
-            "id": 2,
-            "name": "iron man"
-        },
-        {
-            "id": 3,
-            "name": "thor"
-        }
-    ]
-    res.send(films)
-})
-
-router.get("/films/:filmId", function (req, res) {
-    const films = [
-        {
-            "id": 1,
-            "name": "the hulk"
-        },
-        {
-            "id": 2,
-            "name": "iron man"
-        },
-        {
-            "id": 3,
-            "name": "thor"
-        }
-    ]
-    let filmId = req.params.filmId
-    for (let i = 0; i < films.length; i++) {
-        let movie = film[i]
-        if (film.id == filmId)
-            return res.send(film)
-    }
-    res.send("film id does not exist")
-    // console.log('filmId recieve is',filmId)
-})
+//     let requireMovie = movies[movieIndex]
+//     res.send(requireMovie)
 
 
-router.get('/test-you', function (req, res) {
-    res.send('This is the second routes implementation')
-})
+// })
 
-router.get('/give-me-students-data', function (req, res) {
+// router.get("/films", function (req, res) {
+//     const films = [
+//         {
+//             "id": 1,
+//             "name": "the hulk"
+//         },
+//         {
+//             "id": 2,
+//             "name": "iron man"
+//         },
+//         {
+//             "id": 3,
+//             "name": "thor"
+//         }
+//     ]
+//     res.send(films)
+// })
 
-})
+// router.get("/films/:filmId", function (req, res) {
+//     const films = [
+//         {
+//             "id": 1,
+//             "name": "the hulk"
+//         },
+//         {
+//             "id": 2,
+//             "name": "iron man"
+//         },
+//         {
+//             "id": 3,
+//             "name": "thor"
+//         }
+//     ]
+//     let filmId = req.params.filmId
+//     for (let i = 0; i < films.length; i++) {
+//         let movie = film[i]
+//         if (film.id == filmId)
+//             return res.send(film)
+//     }
+//     res.send("film id does not exist")
+//     // console.log('filmId recieve is',filmId)
+// })
+
+
+// router.get('/test-you', function (req, res) {
+//     res.send('This is the second routes implementation')
+// })
+
+// router.get('/give-me-students-data', function (req, res) {
+
+// })
 module.exports = router;
 // adding this comment for no reason
 
